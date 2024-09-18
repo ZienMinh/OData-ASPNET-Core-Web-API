2c36073be88204ea25a9c24237227bff83719b1b
ZienMinh <MinhNLVSE172956@fpt.edu.vn>
2024-09-18 16:41:08 +0700

    fix: connection strings to server

--- ODataBookStore/Program.cs
+++ ODataBookStore/Program.cs
@@ -12,7 +12,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Configure DbContext
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
-	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+	var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+	options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
 });
 
 // Add CORS
